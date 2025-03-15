1709ec1e2ae28aaddf1d65f205ef274d31a08e0f
Mariama <mariamanadiadiouf@gmail.com>
2025-03-15 09:47:07 +0000

    Dropped columns

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -165,19 +165,19 @@ def merge_datasets(df_users, df_veh, df_places, df_caract):
     #df['grav'].replace([2, 3, 4], [0, 1, 1], inplace=True)
     #return df
 
-def replace_values(df):
+#def replace_values(df):
     # Replace values -1 and 0
-    col_to_replace0_na = ["trajet", "catv", "motor"]
-    col_to_replace1_na = ["trajet", "secu1", "catv", "obsm", "motor", "circ", "surf", "situ", "vma", "atm", "col"]
-    df[col_to_replace1_na] = df[col_to_replace1_na].replace(-1, np.nan)
-    df[col_to_replace0_na] = df[col_to_replace0_na].replace(0, np.nan)
-    return df
+ #   col_to_replace0_na = ["trajet", "catv", "motor"]
+  #  col_to_replace1_na = ["trajet", "secu1", "catv", "obsm", "motor", "circ", "surf", "situ", "vma", "atm", "col"]
+   # df[col_to_replace1_na] = df[col_to_replace1_na].replace(-1, np.nan)
+    #df[col_to_replace0_na] = df[col_to_replace0_na].replace(0, np.nan)
+    #return df
 
-def drop_columns(df):
+#def drop_columns(df):
     # Drop columns
-    list_to_drop = ['senc','larrout','actp', 'manv', 'choc', 'nbv', 'prof', 'plan', 'Num_Acc', 'id_vehicule', 'num_veh', 'pr', 'pr1','voie', 'trajet',"secu2", "secu3",'adr', 'v1', 'lartpc','occutc','v2','vosp','locp','etatp', 'infra', 'obs' ]
-    df.drop(list_to_drop, axis=1, inplace=True)
-    return df
+ #   list_to_drop = ['senc','larrout','actp', 'manv', 'choc', 'nbv', 'prof', 'plan', 'Num_Acc', 'id_vehicule', 'num_veh', 'pr', 'pr1','voie', 'trajet',"secu2", "secu3",'adr', 'v1', 'lartpc','occutc','v2','vosp','locp','etatp', 'infra', 'obs' ]
+  #  df.drop(list_to_drop, axis=1, inplace=True)
+   # return df
 
 def drop_lines_with_nan_values(df):
     # Drop lines with NaN values
